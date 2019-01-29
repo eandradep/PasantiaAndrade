4534aa4241350c8fb4425bee40720f6c360f666c
eandradep <33979334+eandradep@users.noreply.github.com>
2019-01-29 12:45:27 -0500

    Listado de Nino casi completo, creacion de Graficas en Radar. Modificaciones

--- app/src/main/java/com/example/pasantiaandrade/Adaptadores/Slider/slide_ninos_listado.kt
+++ app/src/main/java/com/example/pasantiaandrade/Adaptadores/Slider/slide_ninos_listado.kt
@@ -4,25 +4,23 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Typeface
 import android.support.v4.view.PagerAdapter
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.ListView
 import com.bumptech.glide.Glide
 import com.example.pasantiaandrade.Adaptadores.Slider.listas_Adapter.lista_valoracion
 import com.example.pasantiaandrade.Camera
 import com.example.pasantiaandrade.DBHelper.DBHelper
-import com.example.pasantiaandrade.Model.Tab_Observacion
 import com.example.pasantiaandrade.Model.UsuarioNino
 import com.example.pasantiaandrade.R
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.RadarData
 import com.github.mikephil.charting.data.RadarDataSet
 import com.github.mikephil.charting.data.RadarEntry
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import kotlinx.android.synthetic.main.slide_ninos_listado.view.*
-import kotlin.random.Random
 
 class slide_ninos_listado(private var context: Context, private var ListadoNinos:List<UsuarioNino>): PagerAdapter() {
 
@@ -36,7 +34,7 @@ class slide_ninos_listado(private var context: Context, private var ListadoNinos
         return view === o as LinearLayout
     }
 
-    val myArray = arrayOf("M Fina", "M. Gruesa","Habilidad 1","Habilidad 2","Habilidad 3")
+    val myArray = arrayOf("T . V", "T . A","A & C","P","M . F", "M . G")
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -52,12 +50,24 @@ class slide_ninos_listado(private var context: Context, private var ListadoNinos
 
         view.lstObservacionesNinos.adapter = lista_valoracion(context, DBHelper(context).ObtenerValroaciones(this.ListadoNinos[position].TN_id) )
 
-        var radarDataSet = RadarDataSet(Valores(),"Habilidades del Nino")
-        var radarData:RadarData?= RadarData()
-        radarData!!.addDataSet(radarDataSet)
+
+        view.radarCharCaracteristicasNino.webColor = Color.BLACK
+        view.radarCharCaracteristicasNino.webColorInner = Color.BLACK
+        view.radarCharCaracteristicasNino.webLineWidthInner = 2f
         view.radarCharCaracteristicasNino.xAxis.valueFormatter = IndexAxisValueFormatter(myArray)
-        view.radarCharCaracteristicasNino.data=radarData
+        view.radarCharCaracteristicasNino.xAxis.typeface=Tipografia
+        view.radarCharCaracteristicasNino.xAxis.textSize = 13f
+        view.radarCharCaracteristicasNino.yAxis.setDrawLabels(false)
+        view.radarCharCaracteristicasNino.description.isEnabled=false
+        view.radarCharCaracteristicasNino.animateXY(3000,3000,Easing.EasingOption.EaseInOutBack,Easing.EasingOption.EaseOutSine)
+        view.radarCharCaracteristicasNino.data= Grafico(Tipografia)
+        view.radarCharCaracteristicasNino.legend.verticalAlignment=Legend.LegendVerticalAlignment.TOP
+        view.radarCharCaracteristicasNino.legend.horizontalAlignment=Legend.LegendHorizontalAlignment.CENTER
+        view.radarCharCaracteristicasNino.legend.orientation=Legend.LegendOrientation.HORIZONTAL
+        view.radarCharCaracteristicasNino.legend.typeface=Tipografia
+        view.radarCharCaracteristicasNino.legend.textSize=15f
         view.radarCharCaracteristicasNino.invalidate()
+
 
         view.sliderListadoNino.lblListaNinoNombre.typeface = Tipografia
         view.sliderListadoNino.lblListaNinoApellido.typeface = Tipografia
@@ -73,13 +83,29 @@ class slide_ninos_listado(private var context: Context, private var ListadoNinos
         container.addView(view)
         return view
     }
+
+
+    fun Grafico(tipografia: Typeface):RadarData?{
+
+        var radarDataSet = RadarDataSet(Valores(),"Habilidades")
+        radarDataSet.setDrawFilled(true)
+        radarDataSet.lineWidth=3f
+        radarDataSet.color=Color.GRAY
+        radarDataSet.fillColor = Color.GRAY
+        radarDataSet.valueTypeface=tipografia
+        var radarData:RadarData?= RadarData()
+        radarData!!.addDataSet(radarDataSet)
+        return radarData
+    }
+
     fun Valores():ArrayList<RadarEntry>{
         var listado :ArrayList<RadarEntry> = ArrayList<RadarEntry>()
         for (i  in myArray){
-            listado.add(RadarEntry((0..20).random().toFloat()))
+            listado.add(RadarEntry((0..10).random().toFloat()))
         }
         return listado
     }
+
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as LinearLayout)
     }
